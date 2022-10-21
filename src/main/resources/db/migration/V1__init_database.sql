c77c80ccdfacf7e45b37d2d523d6f6eb211e20eb
helen <hhma@alumni.cpp.edu>
2022-10-20 23:10:19 -0400

    create customer

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -1,7 +1,18 @@
+drop table if exists customer;
 drop table if exists order_header cascade;
 
+create table customer
+(
+    id bigint not null auto_increment primary key,
+    phone varchar(10),
+    created_date timestamp,
+    last_modified_date timestamp
+
+) engine = InnoDB;
 create table order_header
 (
     id        bigint not null auto_increment primary key,
-    customer      varchar(255)
+    customer_id      bigint,
+    constraint order_header_fk FOREIGN KEY (customer_id) references customer(id)
 ) engine = InnoDB;
+
