6c7b90362525f50d825bff81541939a827a941b1
helen <hhma@alumni.cpp.edu>
2022-10-20 23:27:08 -0400

    add customer and order header relationship.

--- src/test/java/guru/springframework/orderservice/repositories/OrderHeaderRepositoryTest.java
+++ src/test/java/guru/springframework/orderservice/repositories/OrderHeaderRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,6 +32,7 @@ class OrderHeaderRepositoryTest {
     Product product;
 
     Customer customer;
+
     @BeforeEach
     void setUp() {
         Product newProduct = new Product();
@@ -39,7 +42,7 @@ class OrderHeaderRepositoryTest {
 
         customer = new Customer();
         customer.setPhone("5712401234");
-        customerRepository.saveAndFlush(customer);
+
     }
 
     @Test
@@ -52,20 +55,23 @@ class OrderHeaderRepositoryTest {
         orderLine.setProduct(product);
 
         orderHeader.addOrderLine(orderLine);
+        Set<OrderHeader> orderHeaderSet = new HashSet<>();
+        orderHeaderSet.add(orderHeader);
+        customer.setOrderHeaderSet(orderHeaderSet);
 
         OrderHeader savedOrder = orderHeaderRepository.save(orderHeader);
-
+        Customer savedCustomer = customerRepository.save(customer);
         orderHeaderRepository.flush();
-
-        assertNotNull(savedOrder);
-        assertNotNull(savedOrder.getId());
-        assertNotNull(savedOrder.getOrderLines());
-        assertEquals(savedOrder.getOrderLines().size(), 1);
-
         OrderHeader fetchedOrder = orderHeaderRepository.findById(savedOrder.getId()).get();
         assertTrue(fetchedOrder.getCustomer().getPhone().equals("5712401234"));
         assertNotNull(fetchedOrder);
         assertEquals(fetchedOrder.getOrderLines().size(), 1);
+
+        Customer fetchedCustomer = customerRepository.findById(savedCustomer.getId()).get();
+        Iterator<OrderHeader> fetchedOrderItr = fetchedCustomer.getOrderHeaderSet().iterator();
+        OrderHeader oh = fetchedOrderItr.next();
+        assertTrue(oh.getId()!=null);
+
     }
 
     @Test
