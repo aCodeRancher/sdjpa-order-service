b5b1ce6d072876ffdbd44bd292dfa20cdc172a3b
helen <hhma@alumni.cpp.edu>
2022-10-21 18:08:00 -0400

    add name, email to customer table.

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -5,10 +5,17 @@ create table customer
 (
     id bigint not null auto_increment primary key,
     phone varchar(10),
+    name varchar(50),
+    email varchar(20),
     created_date timestamp,
-    last_modified_date timestamp
+    last_modified_date timestamp,
+    address varchar(30),
+    city varchar(30),
+    state varchar(30),
+    zip_code varchar(30)
 
 ) engine = InnoDB;
+
 create table order_header
 (
     id        bigint not null auto_increment primary key,
