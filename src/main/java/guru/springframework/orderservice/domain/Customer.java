8934f1c6eb3d79e0e9e0ee9fbff7e40462e2890c
helen <hhma@alumni.cpp.edu>
2022-10-21 18:07:31 -0400

    add address and email to the customer.

--- src/main/java/guru/springframework/orderservice/domain/Customer.java
+++ src/main/java/guru/springframework/orderservice/domain/Customer.java
@@ -1,17 +1,68 @@
 package guru.springframework.orderservice.domain;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
+@AttributeOverrides({
+        @AttributeOverride(
+                name = "address.address",
+                column = @Column(name = "address")
+        ),
+        @AttributeOverride(
+                name = "address.city",
+                column = @Column(name = "city")
+        ),
+        @AttributeOverride(
+                name = "address.state",
+                column = @Column(name = "state")
+        ),
+        @AttributeOverride(
+                name = "address.zipCode",
+                column = @Column(name = "zip_code")
+        )
+})
 public class Customer extends BaseEntity{
 
     @OneToMany (mappedBy = "customer", cascade = CascadeType.PERSIST)
     private Set<OrderHeader> orderHeaderSet;
+
+    private String phone;
+
+    private String name;
+
+    private String email;
+
+    @Embedded
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -19,15 +70,17 @@ public class Customer extends BaseEntity{
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Customer customer = (Customer) o;
-        return Objects.equals(phone, customer.phone);
+        return Objects.equals(getId(), customer.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), phone);
+        int result = super.hashCode();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getPhone() != null ? getPhone().hashCode() : 0);
+        return result;
     }
 
-    private String phone;
 
     public String getPhone() {
         return phone;
