659580ed38bf29808f78c547913fddb9887e8148
helen <hhma@alumni.cpp.edu>
2022-10-21 22:19:06 -0400

    add findCustomerByName method

--- src/main/java/guru/springframework/orderservice/repositories/OrderHeaderRepository.java
+++ src/main/java/guru/springframework/orderservice/repositories/OrderHeaderRepository.java
@@ -1,10 +1,16 @@
 package guru.springframework.orderservice.repositories;
 
+import guru.springframework.orderservice.domain.Customer;
 import guru.springframework.orderservice.domain.OrderHeader;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 /**
  * Created by jt on 12/5/21.
  */
 public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Long> {
+
+    @Query(value= " from Customer  as c inner join c.orderHeaderSet as o where c.name =:name ")
+    Customer findCustomerByName(String name);
 }
+
+
