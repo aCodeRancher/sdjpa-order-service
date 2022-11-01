452797516c66746ca15f355ffaf1446c59a13bde
helen <hhma@alumni.cpp.edu>
2022-10-31 23:36:45 -0400

    add customer version test.

---
+++ src/test/java/guru/springframework/orderservice/repositories/CustomerVersionTest.java
@@ -0,0 +1,53 @@
+package guru.springframework.orderservice.repositories;
+
+import guru.springframework.orderservice.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class CustomerVersionTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    Customer savedCustomer ;
+    @BeforeEach
+    void setup(){
+        Customer customer = new Customer();
+        customer.setCustomerName("Test Customer 1");
+        savedCustomer = customerRepository.saveAndFlush(customer);
+    }
+
+    @Test
+    @Rollback(value=false)
+    void updateCustomerName(){
+        assertTrue(savedCustomer.getVersion()==0);
+
+        savedCustomer.setCustomerName("Test Customer 2");
+        Customer savedCustomer1 = customerRepository.saveAndFlush(savedCustomer);
+
+        assertTrue(savedCustomer1.getVersion()==1);
+
+        customerRepository.delete(savedCustomer1);
+    }
+
+    @Test
+    @Rollback(value=false)
+    void noUpdate(){
+        assertTrue(savedCustomer.getVersion()==0);
+        Customer savedCustomer1 = customerRepository.saveAndFlush(savedCustomer);
+
+        assertTrue(savedCustomer1.getVersion()==0);
+         customerRepository.delete(savedCustomer1);
+    }
+
+}
