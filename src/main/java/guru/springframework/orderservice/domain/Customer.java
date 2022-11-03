55f779cc87aa85affca0f994d877828172493afd
helen <hhma@alumni.cpp.edu>
2022-11-03 19:08:26 -0400

    add constraints to Customer fields.

--- src/main/java/guru/springframework/orderservice/domain/Customer.java
+++ src/main/java/guru/springframework/orderservice/domain/Customer.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Version;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.LinkedHashSet;
@@ -26,10 +28,14 @@ public class Customer extends BaseEntity {
 
     @Length(max = 20)
     private String phone;
+
+    @Email
+    @Size(min=7)
     private String email;
 
     @Version
     private Integer version;
+
     @OneToMany(mappedBy = "customer")
     private Set<OrderHeader> orders = new LinkedHashSet<>();
 
